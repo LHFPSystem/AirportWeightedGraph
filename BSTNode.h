3a1b996bf5fd47f8bee2f4b7d0b8f5c798a8b746
Noelia-Fiuba <67407985+Noelia-Fiuba@users.noreply.github.com>
2020-07-23 13:18:25 -0300

    Add files via upload

--- BSTNode.h
+++ BSTNode.h
@@ -1,18 +1,30 @@
+#include<string>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+
 # ifndef ABB_BSTNODE_H
 # define  ABB_BSTNODE_H
+
 
 template < class  T >
 class  BSTNode
 {
 private:
-    T* Datos;
+  	 T* Datos;
     string clave;
     BSTNode <T> * left; 
     BSTNode <T> * right; 
     BSTNode <T> * padre;
 
 public:
-    BSTNode (T* datos,string clave);
+	
+    BSTNode (T* datos,string clave); 
+    
+    
     string get_clave();
     T get_data ();
     void  set_data (T *datos);
@@ -30,7 +42,9 @@ public:
 };
 
 template < class  T >
-BSTNode <T> :: BSTNode (T* datos)
+
+BSTNode <T> :: BSTNode (T* datos,string clave) 
+
 {
     this -> datos = datos;
     this ->clave=clave;
@@ -54,7 +68,7 @@ T BSTNode <T> :: get_clave ()
 
 template < class  T >
 void BSTNode <T> :: set_right (BSTNode <T> * right, BSTNode <T> * parent) {
-    this -> rigtht = derecho;
+    this -> right = right;
     this -> parent = parent;
 }
 
@@ -71,7 +85,7 @@ void BSTNode <T> :: set_left (BSTNode <T> * left, BSTNode <T> * parent) {
 
 template < class  T >
 void BSTNode <T> :: set_parent (BSTNode <T> * parent) {
-    this -> padre = padre;
+    this -> parent = parent;
 }
 
 template < class  T >
@@ -110,12 +124,12 @@ bool BSTNode <T> :: isLeaf () {
 
 template < class  T >
 bool BSTNode <T> :: rightChildOnly () {
-    return ( this -> get_left () == NULL && this -> get_right ()! = NULL );
+    return ( this->get_left() == NULL && this->get_right()!=NULL);
 }
 
 template < class  T >
 bool BSTNode <T> :: leftChildOnly () {
-    return ( this -> get_left ()! = NULL && this -> get_right () == NULL );
+    return ( this->get_left ()!= NULL && this->get_right()==NULL);
 }
 
 # endif  // ABB_BSTNODE_H
