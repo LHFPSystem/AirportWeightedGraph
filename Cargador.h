dbd9d481a245af65e1b99cb439983984ffb8c502
esmiroco <67381964+esmiroco@users.noreply.github.com>
2020-07-27 16:46:14 -0300

    Create Cargador.h

---
+++ Cargador.h
@@ -0,0 +1,42 @@
+#ifndef CARGADOR_H
+#define CARGADOR_H
+#include "Aeropuerto.h"
+#include "BST.h"
+#include "BSTNode.h"
+#include <sstream>
+#include <cstring>
+
+using namespace std;
+
+class Cargador{
+	private:
+		string iata;
+		string nombre;
+		string ciudad;
+		string pais;
+		double superficie;
+		int terminales;
+		int d_nacionales;
+		int d_internacionales;
+		
+		Aeropuerto* p_aeropuerto;
+		BST<Aeropuerto>* p_arbol;
+
+
+	public:
+		//Constructor
+		//PRE: ninguna
+		//POST: crea un objeto diseñado para cargar informacion desde un archivo de texto
+		Cargador();
+
+		//Destructor
+		//PRE: Carga creada
+		//POST:  destruye un ojeto de tipo carga
+		~Cargador();
+
+		void cargar_datos();
+
+};
+
+
+#endif
